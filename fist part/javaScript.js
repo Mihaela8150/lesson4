38c346bc195d0efdc53531ab7778b25301c55716
Mihaela8150 <114097144+Mihaela8150@users.noreply.github.com>
2022-11-09 20:28:45 +0200

    Add files via upload

---
+++ fist part/javaScript.js
@@ -0,0 +1,12 @@
+function changeColor(newColor){
+    const elem =document.getElementById('example');
+    elem.style.color = newColor;
+}
+
+
+
+document.getElementById('example').addEventListener("click",showAlert)
+
+function showAlert() {
+    alert("addEvenListener works")
+}
